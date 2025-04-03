dece109fec024e6bcbaf52e909b090a7b4c6410c
wizdeX <surjith.ap007@gmail.com>
2025-04-03 20:55:43 +0530

    Removed Analytics page

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -57,15 +57,7 @@ const Header = ({ logo }) => {
             </li>
           </Link>
           
-          <Link href="/dashboard/analytics">
-            <li
-              className={`hover:text-black hover:font-bold transition-all cursor-pointer ${
-                path == "/dashboard/analytics" && "text-black font-bold"
-              }`}
-            >
-              Analytics
-            </li>
-          </Link>
+          {/* Analytics link removed */}
         </ul>
         <div className="md:hidden">
           <button onClick={toggleMenu} className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -110,15 +102,7 @@ const Header = ({ logo }) => {
             </li>
           </Link>
           
-          <Link href="/dashboard/analytics">
-            <li
-              className={`hover:text-black hover:font-bold transition-all cursor-pointer ${
-                path == "/dashboard/analytics" && "text-black font-bold"
-              }`}
-            >
-              Analytics
-            </li>
-          </Link>
+          {/* Analytics link removed */}
           </ul>
           </div>
         </div>
